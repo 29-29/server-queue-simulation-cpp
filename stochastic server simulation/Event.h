b147d0fd8e7dae2a24cfad8835d30ce631e8b156
29-29 <ackx29sodo@gmail.com>
2025-05-22 18:59:02 +0800

    Remove Event.cpp and update Event.h with getter and setter methods for member variables

--- stochastic server simulation/Event.h
+++ stochastic server simulation/Event.h
@@ -14,6 +14,16 @@ private:
 public:
 	Event(double _time, EventType _type, int pid): time(_time), type(_type), packetID(pid) {}
 
+	// getters
+	double getTime() const { return time; }
+	EventType getType() const { return type; }
+	int getPacketID() const { return packetID; }
+
+	// setters
+	void setTime(double _time) { time = _time; }
+	void setType(EventType _type) { type = _type; }
+	void setPacketID(int pid) { packetID = pid; }
+
 	bool operator>(Event other) const {
 		return time > other.time;
 	}
